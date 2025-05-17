efb36d0183c5b5dd5ab9e483d6acb1044107475a
Daniel Juape Abad <daniel.juape@gmail.com>
2025-05-17 15:12:45 +0200

    Initial commit - Spring Boot project setup

---
+++ gpt-content-api/src/main/java/com/daniel/gpt_content_api/GptContentApiApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.gpt_content_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptContentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptContentApiApplication.class, args);
+	}
+
+}
---
+++ gpt-content-api/src/test/java/com/daniel/gpt_content_api/GptContentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.gpt_content_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptContentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
